49a814b101eeb81f51420cfd194925cc62421f47
helenagao14 <helena.gao14@gmail.com>
2019-11-24 16:35:38 -0600

    11/24

--- app/src/main/java/com/example/a12daysofchristmas/MainActivity.java
+++ app/src/main/java/com/example/a12daysofchristmas/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.a12daysofchristmas;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.JsonObject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -35,12 +37,30 @@ public class MainActivity extends AppCompatActivity {
         Button playbtn = findViewById(R.id.play);
 
     }
-    private void connect() {
+    private void connect(String search) {
 
         // Make any "loading" UI adjustments you like
         // Use WebApi.startRequest to fetch the games lists
         // In the response callback, call setUpUi with the received data
-        WebApi.startRequest(this, WebApi.API_BASE + "/games", response -> {
+        JsonObject obj = new JsonObject();
+
+
+
+
+        obj.addProperty("q", search);
+        obj.addProperty("type", "track,artist");
+        obj.addProperty("market", "US");
+        obj.addProperty("limit", 10);
+        obj.addProperty("offset", 5);
+        obj.addProperty("Accept:", "application/json");
+        obj.addProperty("Content-Type", "application/json");
+        obj.addProperty("Authorization", "Bearer " +
+                "BQCKl5nCOBq_sEPyTBeNHUe4NNcNmYj8BvsC-" +
+                "O1b3mV5npP466DJaSMJdq38jd0GapA2r5_tSn0yZmkSj" +
+                "Ouib4tT2sL8d3D1afvfPggdDEU4uc06RlAb6XaXHV5VOtJfZHM_c6A4za_yrAfDCPzDpwzQKtXC");
+
+
+        WebApi.startRequest(this, WebApi.API_BASE, Request.Method.GET, obj, response -> {
             // Code in this handler will run when the request completes successfully
             // Do something with the response?
             //the response object will contain the response data as a JsonObject if the endpoint returns a result,
@@ -51,6 +71,14 @@ public class MainActivity extends AppCompatActivity {
             // Maybe notify the user of the error?
 
         });
+    }
+    /**
+     * Populates the games lists UI with data retrieved from the server.
+     *
+     * @param result parsed JSON from the server
+     */
+    private void setUpUi(final JsonObject result) {
+        
     }
 
     @Override
--- app/src/main/java/com/example/a12daysofchristmas/WebApi.java
+++ app/src/main/java/com/example/a12daysofchristmas/WebApi.java
@@ -11,16 +11,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.neovisionaries.ws.client.WebSocket;
-import com.neovisionaries.ws.client.WebSocketAdapter;
-import com.neovisionaries.ws.client.WebSocketException;
-import com.neovisionaries.ws.client.WebSocketFactory;
-import com.neovisionaries.ws.client.WebSocketFrame;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -38,19 +33,12 @@ public final class WebApi {
     private static final String TAG = "WebApi";
 
     /** The URL at which the server is hosted. */
-    static final String API_BASE = "https://cs125-cloud.cs.illinois.edu/Fall2019-MP";
+    static final String API_BASE = "https://api.spotify.com/v1/search";
 
-    /** The URL at which the webserver socket is hosted. */
-    static final String WEBSOCKET_BASE = "wss://cs125-cloud.cs.illinois.edu/Fall2019-MP";
 
     /** The HTTP status code for Bad Request. */
     private static final int HTTP_BAD_REQUEST = 400;
 
-    /** Timeout (milliseconds) for connecting to a game websocket. */
-    private static final int WEBSOCKET_CONNECTION_TIMEOUT = 5000;
-
-    /** Interval (milliseconds) at which to make sure the websocket is still connected. */
-    private static final int WEBSOCKET_PING_INTERVAL = 60000;
 
     /** The Volley request queue for the application, or null if the queue hasn't been set up yet. */
     private static RequestQueue requestQueue;
@@ -88,11 +76,13 @@ public final class WebApi {
             Log.i(TAG, "Creating request queue");
             requestQueue = Volley.newRequestQueue(context.getApplicationContext());
         }
+        /**
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null) {
             Log.e(TAG, "startRequest called before Firebase Authentication login");
             throw new IllegalStateException("No user is logged in");
         }
+         */
         Response.Listener<String> serverResponseListener = stringResponse -> {
             if (stringResponse == null || stringResponse.isEmpty()) {
                 Log.i(TAG, "Delivering empty response from " + url);
@@ -119,97 +109,6 @@ public final class WebApi {
                 errorListener.onErrorResponse(error);
             }
         };
-        user.getIdToken(false).addOnSuccessListener(result ->
-                requestQueue.add(new StringRequest(method, url, serverResponseListener, serverErrorListener) {
-                    {
-                        Log.i(TAG, "startRequest creating Volley request (received Firebase ID token)");
-                    }
-                    @Override
-                    public byte[] getBody() throws AuthFailureError {
-                        if (body == null) {
-                            return super.getBody();
-                        } else {
-                            return body.toString().getBytes();
-                        }
-                    }
-                    @Override
-                    public String getBodyContentType() {
-                        if (body == null) {
-                            return super.getBodyContentType();
-                        } else {
-                            return "application/json";
-                        }
-                    }
-                    @Override
-                    public Map<String, String> getHeaders() {
-                        return Collections.singletonMap("Firebase-Token", result.getToken());
-                    }
-                })
-        ).addOnFailureListener(e -> errorListener.onErrorResponse(new VolleyError(e)));
-        Log.i(TAG, "startRequest started getIdToken");
-    }
-
-    /**
-     * Connects to a websocket.
-     * @param url the websocket endpoint
-     * @param dataListener receiver for data messages
-     * @param onCreatedListener callback to run with the websocket when it is created
-     * @param connectionLostListener callback to run if the connection is lost
-     * @param errorListener callback to run if an error occurs during the initial connection
-     */
-    public static void connectWebSocket(final String url, final Consumer<JsonObject> dataListener,
-                                        final Consumer<WebSocket> onCreatedListener,
-                                        final Runnable connectionLostListener,
-                                        final Consumer<Throwable> errorListener) {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user == null) {
-            Log.e(TAG, "connectWebSocket called before Firebase Authentication login");
-            throw new IllegalStateException("No user is logged in");
-        }
-        user.getIdToken(false).addOnSuccessListener(result -> {
-            Log.i(TAG, "connectWebSocket received Firebase ID token");
-            WebSocketFactory factory = new WebSocketFactory();
-            factory.setConnectionTimeout(WEBSOCKET_CONNECTION_TIMEOUT);
-            try {
-                WebSocket socket = factory.createSocket(url);
-                socket.setPingInterval(WEBSOCKET_PING_INTERVAL);
-                socket.addHeader("Firebase-Token", result.getToken());
-                socket.addListener(new WebSocketAdapter() {
-                    private boolean disconnectedDueToError = false;
-                    @Override
-                    public void onTextMessage(final WebSocket websocket, final String text) {
-                        dataListener.accept(jsonParser.parse(text).getAsJsonObject());
-                    }
-                    @Override
-                    public void onError(final WebSocket websocket, final WebSocketException cause) {
-                        disconnectedDueToError = true;
-                    }
-                    @Override
-                    public void onDisconnected(final WebSocket websocket, final WebSocketFrame serverCloseFrame,
-                                               final WebSocketFrame clientCloseFrame, final boolean closedByServer) {
-                        if (disconnectedDueToError) {
-                            Log.i(TAG, "Delivering connection-lost error for " + url);
-                            connectionLostListener.run();
-                        } else {
-                            Log.i(TAG, "Websocket closed to " + url);
-                        }
-                    }
-                    @Override
-                    public void onConnectError(final WebSocket websocket, final WebSocketException exception) {
-                        disconnectedDueToError = false;
-                        Log.i(TAG, "Delivering websocket connection error for " + url);
-                        errorListener.accept(exception);
-                    }
-                });
-                socket.connectAsynchronously();
-                Log.i(TAG, "Delivering websocket instance for " + url);
-                onCreatedListener.accept(socket);
-            } catch (IOException e) {
-                Log.i(TAG, "Delivering websocket setup error for " + url);
-                errorListener.accept(e);
-            }
-        }).addOnFailureListener(errorListener::accept);
-        Log.i(TAG, "connectWebSocket started getIdToken");
     }
 
 }
