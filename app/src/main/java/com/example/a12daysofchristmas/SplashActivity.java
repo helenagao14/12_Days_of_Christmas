d31d411f27354e7bcd75d22ece0dc90843ed3ec2
Shoshanna Perchanok <sammy87864@gmail.com>
2019-11-26 15:27:37 -0600

    got the login screen

---
+++ app/src/main/java/com/example/a12daysofchristmas/SplashActivity.java
@@ -0,0 +1,84 @@
+package com.example.a12daysofchristmas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String REDIRECT_URI = "com.example.a12daysofchristmas://callback";
+    private static final String CLIENT_ID = "8624defd6fe647dcb5662b951d4b7cc9";
+    private static final int REQUEST_CODE = 1337;
+    private static final String SCOPES = "user-read-recently-played,user-library-modify,user-read-email,user-read-private";
+
+    private SharedPreferences.Editor editor;
+    private SharedPreferences msharedPreferences;
+
+    private RequestQueue queue;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+
+
+        authenticateSpotify();
+
+        msharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        queue = Volley.newRequestQueue(this);
+    }
+
+    private void authenticateSpotify() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{SCOPES});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    editor = getSharedPreferences("SPOTIFY", 0).edit();
+                    editor.putString("token", response.getAccessToken());
+                    Log.d("STARTING", "GOT AUTH TOKEN");
+                    editor.apply();
+                    waitForUserInfo();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+
+    private void waitForUserInfo() {
+    }
+}
---
+++ app/src/main/java/com/example/a12daysofchristmas/User.java
@@ -0,0 +1,9 @@
+package com.example.a12daysofchristmas;
+
+public class User {
+    public String birthdate;
+    public String country;
+    public String display_name;
+    public String email;
+    public String id;
+}
